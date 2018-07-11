0aec597a8568a90c951e8e758b39803454c0b4aa
JamesWang <jame2408@gmail.com>
2018-07-11 19:05:23 +0800

    非同步載入頁面各區塊 Partial View。

---
+++ AsyncPartialView/Controllers/AsyncPartialViewDemoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AsyncPartialView.Controllers
+{
+    public class AsyncPartialViewDemoController : Controller
+    {
+        // GET: AsyncPartialViewDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Area_A()
+        {
+            Thread.Sleep(1000);
+            return PartialView("_Area_A", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ffff"));
+        }
+
+        public async Task<ActionResult> Area_B()
+        {
+            Thread.Sleep(2000);
+            return PartialView("_Area_B", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ffff"));
+        }
+
+        public async Task<ActionResult> Area_C()
+        {
+            Thread.Sleep(3000);
+            return PartialView("_Area_C", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ffff"));
+        }
+    }
+}
